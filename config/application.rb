54d9aa2f2797a23488450776db951fc10eac4eb8
Tut Wal <tutkoang@gmail.com>
2018-12-13 19:13:25 -0700

    Added guest user for current_user

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,6 @@ Bundler.require(*Rails.groups)
 
 module WalSystems
   class Application < Rails::Application
-    config.action_controller
-        permit_all_parameters = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
