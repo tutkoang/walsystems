986efe302da323ce7da5adedace7fcd333d55b03
Tut Wal <tutkoang@gmail.com>
2018-12-13 22:45:48 -0700

    Implemented partials with local dat for porfolio form and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -29,14 +29,14 @@ end
 end
 
   def edit
-  	@portfolio_item = Portfolio.find(params[:id])	
+  	@portfolio_item = Portfolio.find(params[:id])
   end
 
    def update
    	@portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update()
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
       else
         format.html { render :edit }
