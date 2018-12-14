7b9d0c0cfd80913665b987240888bc534140f0dc
Tut Wal <tutkoang@gmail.com>
2018-12-14 00:07:58 -0700

    Implemented content tat helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,13 @@ module ApplicationHelper
 	(link_to "Register", new_user_registration_path) +
 	"<br>".html_safe +
     (link_to "Login", new_user_session_path)
-    	end
-	end
+    end
+end
+
+def source_helper(layout_name)
+	if session[:source]
+		greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+    	content_tag(:p, greeting, class: "source-greeting")
+    end	
+  end
 end
