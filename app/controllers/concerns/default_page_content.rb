859ef6150628772514b377a832b2c12ef1912de0
Tut Wal <tutkoang@gmail.com>
2018-12-13 21:08:29 -0700

    Added nav bar and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,13 @@
+module DefaultPageContent
+		extend ActiveSupport::Concern
+
+	included do 
+		before_action :set_page_defaults
+	end
+ protected
+
+ def set_page_defaults
+	@page_title = "WalSystems | My Portfolio Website"
+	@seo_keywords = "Tut Koang portfolio" 	
+ end
+end
