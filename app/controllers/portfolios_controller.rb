0807ee0326663405ad6bb1dd7ad5e760e800888a
Tut Wal <tutkoang@gmail.com>
2018-12-24 14:48:37 -0700

    Implemented initial file uploading via carrierwave for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -72,6 +72,8 @@ end
     params.require(:portfolio).permit(:title, 
                                       :subtitle, 
                                       :body, 
+                                      :main_image,
+                                      :thumb_image,
                                       technologies_attributes: [:name]
                                       )
     
