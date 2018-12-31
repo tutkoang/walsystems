036739af71e62ae14950368a790b0b86f3cf5cb7
Tut Wal <tutkoang@gmail.com>
2018-12-31 12:11:10 -0700

    Added nav link for Tech-News

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,6 +20,49 @@ def source_helper(layout_name)
     @copyright = YViewTool::Renderer.copyright 'WalSystems', 'All right reserved'
   end
 
+    def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+      {
+        url: tech_news_path,
+        title: 'Tech News'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
   def alerts
     alert = (flash[:alert] || flash[:error] || flash[:notice])
 
