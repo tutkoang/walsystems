618bac137d0d78f2a5a8a5118006584da6ef4ba3
Tut Wal <tutkoang@gmail.com>
2019-01-04 12:15:23 -0700

    Updated secret base key call

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ Bundler.require(*Rails.groups)
 module WalSystems
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
-    config.secret_key_base = ENV ["SECRET_KEY_BASE"]
+     config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
